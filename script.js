24f2ff29bea248bc869fa1437ea81dd48c7a707e
liu-samuel <liusamuelt@gmail.com>
2022-06-11 01:16:29 -0400

    Functional game, now make it look good

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let draws = 0;
 let rockButton = document.querySelector(".rock");
 let paperButton = document.querySelector(".paper");
 let scissorsButton = document.querySelector(".scissors");
+let buttons = document.getElementsByTagName("button");
 
 let result = document.querySelector(".result");
 let picks = document.querySelector(".picks");
@@ -21,35 +22,34 @@ let restart = document.querySelector(".restart");
 
 rockButton.addEventListener('click', () => {
     playRound('rock', computerPlay());
-    if (playerWins == 5|| computerWins == 5) {
-        rockButton.disabled = true;
-        paperButton.disabled = true;
-        scissorsButton.disabled = true;
+    if (computerWins == 5 || playerWins == 5) {
+        for (let i = 0; i < 3; i++) {
+            buttons[i].disabled = true;
+        }
     }
 });
 
 paperButton.addEventListener('click', () => {
     playRound('paper', computerPlay());
-    if (playerWins == 5|| computerWins == 5) {
-        rockButton.disabled = true;
-        paperButton.disabled = true;
-        scissorsButton.disabled = true;
-    }
+    if (computerWins == 5 || playerWins == 5) {
+        for (let i = 0; i < 3; i++) {
+            buttons[i].disabled = true;
+        }
+     }
+     
 });
 
 scissorsButton.addEventListener('click', () => {
     playRound('scissors', computerPlay());
-    if (playerWins == 5|| computerWins == 5) {
-        rockButton.disabled = true;
-        paperButton.disabled = true;
-        scissorsButton.disabled = true;
-    }
+    if (computerWins == 5 || playerWins == 5) {
+        for (let i = 0; i < 3; i++) {
+            buttons[i].disabled = true;
+        }
+     }
+     
 });
 
 restart.addEventListener('click', () => {
-    playerWins = 0;
-    computerWins = 0;
-    draws = 0;
     window.location.reload();
 });
 
@@ -73,77 +73,70 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection == "rock") {
             picksText.textContent = "You picked rock and the computer picked rock";
             picks.appendChild(picksText);
-            resultText.textContent = "Draw!";
+            resultText.textContent = "Result: Draw!";
             result.appendChild(resultText)
             draws += 1;
-            cScoreText.textContent = String(computerWins);
-            pScoreText.textContent = String(playerWins);
+            cScoreText.textContent = "Computer: " + computerWins;
+            pScoreText.textContent = "Player: " + playerWins;
             scores.appendChild(cScoreText);
             scores.appendChild(pScoreText);
-            return "draw";
         }
         else if (computerSelection == "paper") {
             picksText.textContent = "You picked rock and the computer picked paper";
             picks.appendChild(picksText);
-            resultText.textContent = "You lose! Paper wraps rock";
+            resultText.textContent = "Result: You lose! Paper wraps rock";
             result.appendChild(resultText)
             computerWins += 1;
-            cScoreText.textContent = String(computerWins);
-            pScoreText.textContent = String(playerWins);
+            cScoreText.textContent = "Computer: " + computerWins;
+            pScoreText.textContent = "Player: " + playerWins;
             scores.appendChild(cScoreText);
             scores.appendChild(pScoreText);
-            return "loss";
         }
         else if (computerSelection == "scissors"){
             picksText.textContent = "You picked rock and the computer picked scissors";
             picks.appendChild(picksText);
-            resultText.textContent = "You win! Rock smashes scissors";
+            resultText.textContent = "Result: You win! Rock smashes scissors";
             result.appendChild(resultText)
             playerWins += 1;
-            cScoreText.textContent = String(computerWins);
-            pScoreText.textContent = String(playerWins);
+            cScoreText.textContent = "Computer: " + computerWins;
+            pScoreText.textContent = "Player: " + playerWins;
             scores.appendChild(cScoreText);
-            scores.appendChild(pScoreText);
-            return "win";
-        }
+            scores.appendChild(pScoreText);        }
     }
 
     else if (playerSelection == "paper") {
         if  (computerSelection == "paper") {
             picksText.textContent = "You picked paper and the computer picked paper";
             picks.appendChild(picksText);
-            resultText.textContent = "Draw!";
+            resultText.textContent = "Result: Draw!";
             result.appendChild(resultText);
             draws += 1;
-            cScoreText.textContent = String(computerWins);
-            pScoreText.textContent = String(playerWins);
+            cScoreText.textContent = "Computer: " + computerWins;
+            pScoreText.textContent = "Player: " + playerWins;
             scores.appendChild(cScoreText);
             scores.appendChild(pScoreText);
-            return "draw";
             }
         else if (computerSelection == "scissors") {
             picksText.textContent = "You picked paper and the computer picked scissors";
             picks.appendChild(picksText);
-            resultText.textContent = "You lose! Scissors cut paper";
+            resultText.textContent = "Result: You lose! Scissors cut paper";
             result.appendChild(resultText);
             computerWins += 1;
-            cScoreText.textContent = String(computerWins);
-            pScoreText.textContent = String(playerWins);
+            cScoreText.textContent = "Computer: " + computerWins;
+            pScoreText.textContent = "Player: " + playerWins;
             scores.appendChild(cScoreText);
             scores.appendChild(pScoreText);
-            return "loss";
         }
         else {
             picksText.textContent = "You picked paper and the computer picked rock";
             picks.appendChild(picksText);
-            resultText.textContent = "You win! Paper wraps rock";
+            resultText.textContent = "Result: You win! Paper wraps rock";
             result.appendChild(resultText);
             playerWins += 1;
-            cScoreText.textContent = String(computerWins);
-            pScoreText.textContent = String(playerWins);
+            cScoreText.textContent = "Computer: " + computerWins;
+            pScoreText.textContent = "Player: " + playerWins;
             scores.appendChild(cScoreText);
             scores.appendChild(pScoreText);
-            return "win";
         }
     }
 
@@ -151,39 +144,37 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection == "scissors") {
             picksText.textContent = "You picked scissors and the computer picked scissors";
             picks.appendChild(picksText);
-            resultText.textContent = "Draw!";
+            resultText.textContent = "Result: Draw!";
             result.appendChild(resultText);
             draws += 1;
-            cScoreText.textContent = String(computerWins);
-            pScoreText.textContent = String(playerWins);
+            cScoreText.textContent = "Computer: " + computerWins;
+            pScoreText.textContent = "Player: " + playerWins;
             scores.appendChild(cScoreText);
             scores.appendChild(pScoreText);
-            return "draw";
         }
         else if (computerSelection == "paper") {
             picksText.textContent = "You picked scissors and the computer picked paper";
             picks.appendChild(picksText);
-            resultText.textContent = "You win! Scissors cut paper";
+            resultText.textContent = "Result: You win! Scissors cut paper";
             result.appendChild(resultText);
             playerWins += 1;
-            cScoreText.textContent = String(computerWins);
-            pScoreText.textContent = String(playerWins);
+            cScoreText.textContent = "Computer: " + computerWins;
+            pScoreText.textContent = "Player: " + playerWins;
             scores.appendChild(cScoreText);
             scores.appendChild(pScoreText);
-            return "win";
         }
         else {
             picksText.textContent = "You picked scissors and the computer picked rock";
             picks.appendChild(picksText);
-            resultText.textContent = "You lose! Rock smashes scissors";
+            resultText.textContent = "Result: You lose! Rock smashes scissors";
             result.appendChild(resultText);
             computerWins += 1;
-            cScoreText.textContent = String(computerWins);
-            pScoreText.textContent = String(playerWins);
+            cScoreText.textContent = "Computer: " + computerWins;
+            pScoreText.textContent = "Player: " + playerWins;
             scores.appendChild(cScoreText);
             scores.appendChild(pScoreText);
-            return "loss";
         }
     }
 }
+
 
